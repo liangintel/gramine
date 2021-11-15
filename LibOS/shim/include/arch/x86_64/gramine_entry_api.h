c85428f8b832c897988071ba29d5d2550aa6b7af
Paweł Marczewski <pawel@invisiblethingslab.com>
2021-11-15 15:16:29 +0100

    fixup! [LibOS] Use the syscall interface for custom calls
    Signed-off-by: Paweł Marczewski <pawel@invisiblethingslab.com>

--- LibOS/shim/include/arch/x86_64/gramine_entry_api.h
+++ LibOS/shim/include/arch/x86_64/gramine_entry_api.h
@@ -48,7 +48,7 @@ enum {
 };
 
 /* Magic syscall number for Gramine custom calls */
-#define GRAMINE_CUSTOM_SYSCALL_NR ((unsigned long)-1)
+#define GRAMINE_CUSTOM_SYSCALL_NR 0x100000
 
 static inline int gramine_call(int number, unsigned long arg1, unsigned long arg2) {
     int ret;
